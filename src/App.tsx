8548c75172be4940e7079fc089872273f56b8f78
dergash <dergachev@hotmail.com>
2021-08-09 10:54:44 +0300

    feat(store): persistent redux-toolkit store

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,30 @@ import {Stack} from './navigation/navigation';
 import {AuthContext} from './context/AuthContext';
 import HomeTabs from './features/Home/HomeTabs';
 import {useAuthContext} from './features/Auth/hooks/useAuthContext';
+import {Provider} from 'react-redux';
+import {persistor, store} from './utils/store/configureStore';
+import {PersistGate} from 'redux-persist/integration/react';
 
 const App = () => {
   const authContext = useAuthContext();
 
   return (
-    <AuthContext.Provider value={authContext}>
-      <Stack.Navigator>
-        {authContext?.accessToken || authContext?.idToken ? (
-          <Stack.Screen name="Home" component={HomeTabs} />
-        ) : (
-          <Stack.Screen
-            name="Auth"
-            component={AuthScreen}
-            options={{headerShown: false, animationTypeForReplace: 'pop'}}
-          />
-        )}
-      </Stack.Navigator>
-    </AuthContext.Provider>
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null} />
+      <AuthContext.Provider value={authContext}>
+        <Stack.Navigator>
+          {authContext?.accessToken || authContext?.idToken ? (
+            <Stack.Screen name="Home" component={HomeTabs} />
+          ) : (
+            <Stack.Screen
+              name="Auth"
+              component={AuthScreen}
+              options={{headerShown: false, animationTypeForReplace: 'pop'}}
+            />
+          )}
+        </Stack.Navigator>
+      </AuthContext.Provider>
+    </Provider>
   );
 };
 
--- src/features/Home/HomeScreen.tsx
+++ src/features/Home/HomeScreen.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import useFetch from '../../hooks/useFetch';
 import Spinner from '../Auth/components/Spinner';
-import {Survey} from '../../interfaces/survey';
 import SurveyCard from '../../components/SurveyCard';
 import ListContainer from '../../components/ListContainer';
 import ListInfoCaption from '../../components/ListInfoCaption';
+import {useAllSurveysQuery} from '../Survey/surveyService';
 
 function HomeScreen() {
   // const [data, error, isLoading] = useFetch(
   //   'https://gnsa-dev.i.daimler.com/api/v1/data/smaudit/smAudit?_limit=5&_page=1',
   // );
-  const {data, error} = useFetch<Survey[]>( // fixme temp hook
-    'http://localhost:8080/api/v1/mobile-audit-execution/surveys',
-    // 'https://gnsa-dev.i.daimler.com/api/v1/authenticate',
-    // 'http://localhost:8080/api/v1/auth/authenticate',
-    // 'https://sso-int.daimler.com/idp/userinfo.openid',
-  );
-  if (!data) {
+  const {data, error, isLoading} = useAllSurveysQuery('');
+
+  if (isLoading) {
     return <Spinner inProgress />;
   }
   console.log('-------');
@@ -25,7 +20,7 @@ function HomeScreen() {
   return (
     <ListContainer>
       <ListInfoCaption
-        leftCaption={`All Surveys · ${data.length}`}
+        leftCaption={`All Surveys · ${data?.length ?? 0}`}
         rightCaption="Updated /date here/"
       />
       {data?.map(survey => {
