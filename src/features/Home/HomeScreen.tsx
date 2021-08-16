a9f83dcfc5f9dad7d6fd0cb54b826180bbcb3c38
dergash <dergachev@hotmail.com>
2021-08-16 09:49:38 +0300

    feat(survey-list): use virtualized list for surveys

--- src/features/Home/HomeScreen.tsx
+++ src/features/Home/HomeScreen.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import Spinner from '../Auth/components/Spinner';
 import SurveyCard from '../../components/SurveyCard';
-import ListContainer from '../../components/ListContainer';
 import ListInfoCaption from '../../components/ListInfoCaption';
 import {useAllSurveysQuery} from '../Survey/surveyService';
+import {FlatList} from 'react-native-gesture-handler';
+import {Survey} from '../../interfaces/survey';
+import {StyleSheet, View} from 'react-native';
 
 function HomeScreen() {
-  // const [data, error, isLoading] = useFetch(
-  //   'https://gnsa-dev.i.daimler.com/api/v1/data/smaudit/smAudit?_limit=5&_page=1',
-  // );
   const {data, error, isLoading} = useAllSurveysQuery('');
+
+  const keyExtractor = useCallback((item: Survey) => item.id, []);
+
+  const renderItem = useCallback(({item}: {item: Survey}) => {
+    return <SurveyCard survey={item} key={item.id} />;
+  }, []);
+
   if (isLoading) {
     return <Spinner inProgress />;
   }
@@ -17,16 +23,26 @@ function HomeScreen() {
   console.log('data', data);
   console.log('error', error);
   return (
-    <ListContainer>
+    <View style={styles.scrollView}>
       <ListInfoCaption
         leftCaption={`All Surveys · ${data?.length ?? 0}`}
         rightCaption="Updated /date here/"
       />
-      {data?.map(survey => {
-        return <SurveyCard survey={survey} key={survey.id} />;
-      })}
-    </ListContainer>
+      <FlatList
+        data={data}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    paddingLeft: '4%',
+    paddingRight: '4%',
+    height: '100%',
+  },
+});
 
 export default React.memo(HomeScreen);
