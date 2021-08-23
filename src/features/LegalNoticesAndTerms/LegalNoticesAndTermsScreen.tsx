f8ed52ff2cf569f4aea870d963a645c74c676695
Fedor Gubarev <gubarevfyodor@gmail.com>
2021-08-23 10:28:59 +0300

    fix(legal-notices-screen): style fix

--- src/features/LegalNoticesAndTerms/LegalNoticesAndTermsScreen.tsx
+++ src/features/LegalNoticesAndTerms/LegalNoticesAndTermsScreen.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {Caption, Headline, Paragraph, Text} from 'react-native-paper';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import themeConfig from '../../../themeConfig';
-import {ScrollView} from 'react-native-gesture-handler';
 
 const providerData = {
   'Name:': 'Mercedes-Benz AG',
@@ -16,51 +15,48 @@ const providerData = {
 function LegalNoticesAndTermsScreen() {
   return (
     <SafeAreaView>
-      <ScrollView>
-        <View style={styles.container}>
-          <View>
-            <View style={styles.section}>
-              <Headline style={styles.header}>Legal notices and Terms</Headline>
-              <Paragraph style={styles.paragraph}>
-                The GNSA is provided by the Mercedes-Benz AG to Daimler
-                wholesale companies. It is used to evaluate the fulfillment of
-                retail standards - agreed upon in the contracts - at dealerships
-                which are part of the Mercedes-Benz AG retail network.
-              </Paragraph>
-              <Paragraph style={styles.paragraph}>
-                The user is able to download Audits from the central server,
-                evaluate standards and then upload the results again. The GNSA
-                is solely provided for this specific purpose.
-              </Paragraph>
-              <Paragraph>
-                Usage is restricted to authorized persons only. By accessing the
-                GNSA the user agrees to the Mercedes-Benz AG terms and
-                conditions.
-              </Paragraph>
-            </View>
-            <View style={styles.section}>
-              <Headline style={styles.header}>Provider</Headline>
-              <View style={styles.table}>
-                <View style={styles.leftColumn}>
-                  {Object.keys(providerData).map(i => (
-                    <Text style={labelStyle} key={i}>
-                      {i}
-                    </Text>
-                  ))}
-                </View>
-                <View style={styles.rightColumn}>
-                  {Object.values(providerData).map(i => (
-                    <Text style={styles.cell} key={i}>
-                      {i}
-                    </Text>
-                  ))}
-                </View>
+      <View style={styles.container}>
+        <View>
+          <View style={styles.section}>
+            <Headline style={styles.header}>Legal notices and Terms</Headline>
+            <Paragraph style={styles.paragraph}>
+              The GNSA is provided by the Mercedes-Benz AG to Daimler wholesale
+              companies. It is used to evaluate the fulfillment of retail
+              standards - agreed upon in the contracts - at dealerships which
+              are part of the Mercedes-Benz AG retail network.
+            </Paragraph>
+            <Paragraph style={styles.paragraph}>
+              The user is able to download Audits from the central server,
+              evaluate standards and then upload the results again. The GNSA is
+              solely provided for this specific purpose.
+            </Paragraph>
+            <Paragraph>
+              Usage is restricted to authorized persons only. By accessing the
+              GNSA the user agrees to the Mercedes-Benz AG terms and conditions.
+            </Paragraph>
+          </View>
+          <View style={styles.section}>
+            <Headline style={styles.header}>Provider</Headline>
+            <View style={styles.table}>
+              <View style={styles.leftColumn}>
+                {Object.keys(providerData).map(i => (
+                  <Text style={labelStyle} key={i}>
+                    {i}
+                  </Text>
+                ))}
+              </View>
+              <View style={styles.rightColumn}>
+                {Object.values(providerData).map(i => (
+                  <Text style={styles.cell} key={i}>
+                    {i}
+                  </Text>
+                ))}
               </View>
             </View>
           </View>
-          <Caption>Mercedes-Benz AG ©</Caption>
         </View>
-      </ScrollView>
+        <Caption>Mercedes-Benz AG ©</Caption>
+      </View>
     </SafeAreaView>
   );
 }
