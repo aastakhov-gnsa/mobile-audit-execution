ad7229da7c627f2aca2e8872bb18fb64bee08377
Fedor Gubarev <gubarevfyodor@gmail.com>
2021-09-21 13:31:19 +0300

    feat(android): app icons are added; `authTest` is removed

--- src/features/authTest/components/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Button} from './Button';
-export {default as ButtonContainer} from './ButtonContainer';
-export {default as Heading} from './Heading';
-export {default as Page} from './Page';
