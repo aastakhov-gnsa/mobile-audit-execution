53d0bc4c982f5f714f9efe13ce4d2ee6aef03746
Fedor Gubarev <gubarevfyodor@gmail.com>
2021-10-10 12:47:48 +0300

    feat(audit-details): progress bar is added

---
+++ src/components/AuditProgress.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {ProgressBar, useTheme} from 'react-native-paper';
+import Typography from './Typography';
+import ItemWrapper from './ItemWrapper';
+import {useSelector} from '../utils/store/configureStore';
+
+interface AuditProgressProps {
+  surveyId: string;
+}
+
+function AuditProgress({surveyId}: AuditProgressProps) {
+  const {colors} = useTheme();
+  const styles = makeStyles(colors);
+  const standards = useSelector(state => state.evaluation[surveyId]?.standards);
+  const completed = standards?.reduce(
+    (acc, current) => (current?.status ? acc + 1 : acc),
+    0,
+  );
+  const all = standards?.length;
+  return (
+    <>
+      <ItemWrapper paddingValue={[0, 14]}>
+        <ProgressBar
+          color={colors.primary}
+          style={styles.bar}
+          progress={completed / all}
+        />
+      </ItemWrapper>
+      <Typography size="Body 1">
+        {completed} of {all}
+      </Typography>
+    </>
+  );
+}
+
+export default React.memo(AuditProgress);
+
+const makeStyles = (colors: ReactNativePaper.ThemeColors) =>
+  StyleSheet.create({
+    bar: {
+      height: 27,
+      backgroundColor: colors.onBackground10,
+    },
+  });
